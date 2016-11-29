5d18db8ea8056313eafeee7b1acba47d2a64b73f
NobodyNada <jkeller234@icloud.com>
2016-11-29 12:03:58 -0800

    Update lws.h

--- lws.h
+++ lws.h
@@ -1,4 +1,6 @@
 #include <libwebsockets.h>
 
+//Some #define'd constants are unavailable in Swift; add them here to export them.
 const int SWIFT_LWS_PRE_PADDING = LWS_SEND_BUFFER_PRE_PADDING;
 const int SWIFT_LWS_POST_PADDING = LWS_SEND_BUFFER_POST_PADDING;
+//If more constants have the problem, add them here.
