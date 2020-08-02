7b56707ba3d28e92a1f35cfcee159eabdc3160ae
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2020-08-02 13:28:40 -0700

    typo

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     products: [.library(name: "Clibwebsockets", targets: ["Clibwebsockets"])],
     dependencies: [
         .package(url: "git://github.com/NobodyNada/COpenSSL", from: "0.1.0"),
-        .package(url: "git://github.com/NobodyNada/Clibuv", from: "0.0.0")
+        .package(url: "git://github.com/NobodyNada/Clibuv", from: "0.0.0"),
         .package(url: "git://github.com/NobodyNada/Clibevent", from: "0.0.0")
     ],
     targets: [
