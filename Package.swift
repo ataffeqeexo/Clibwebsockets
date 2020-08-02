6927065f9267b846b02f92540a1aa95be86428a4
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2020-08-02 13:19:21 -0700

    add libuv dependency

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ let package = Package(
     name: "Clibwebsockets",
     products: [.library(name: "Clibwebsockets", targets: ["Clibwebsockets"])],
     dependencies: [
-        .package(url: "git://github.com/NobodyNada/COpenSSL", from: "0.1.0")
+        .package(url: "git://github.com/NobodyNada/COpenSSL", from: "0.1.0"),
+        .package(url: "git://github.com/NobodyNada/Clibuv", from: "0.0.0")
     ],
     targets: [
         .systemLibrary(
