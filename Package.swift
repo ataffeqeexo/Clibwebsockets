d45bc8b5d70a12f9b5a8dda59b903f3803114530
Alex Taffe <65547595+ataffeqeexo@users.noreply.github.com>
2020-10-14 08:15:00 -0400

    Fix path issue and update dependency version

--- Package.swift
+++ Package.swift
@@ -6,14 +6,13 @@ let package = Package(
     name: "Clibwebsockets",
     products: [.library(name: "Clibwebsockets", targets: ["Clibwebsockets"])],
     dependencies: [
-        .package(url: "git://github.com/NobodyNada/COpenSSL", from: "0.1.0"),
-        .package(url: "git://github.com/NobodyNada/Clibuv", from: "0.0.0"),
-        .package(url: "git://github.com/NobodyNada/Clibevent", from: "0.0.0")
+        .package(url: "git://github.com/ataffeqeexo/COpenSSL", from: "0.1.0"),
+        .package(url: "git://github.com/ataffeqeexo/Clibuv", from: "0.0.0"),
+        .package(url: "git://github.com/ataffeqeexo/Clibevent", from: "0.0.0")
     ],
     targets: [
         .systemLibrary(
             name: "Clibwebsockets",
-            path: "/usr/local/opt/qeexo/libwebsocket",
             pkgConfig: "libwebsockets",
             providers: [.brew(["libwebsockets"]), .apt(["libwebsockets-dev"])]
         )
