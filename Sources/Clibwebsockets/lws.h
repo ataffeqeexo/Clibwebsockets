9ee51f07c12c524d136c49d5c1254b4bf436f9db
Alex Taffe <alex.taffe@qeexo.com>
2020-10-14 10:49:56 -0400

    add conditional macOS import

--- Sources/Clibwebsockets/lws.h
+++ Sources/Clibwebsockets/lws.h
@@ -4,7 +4,11 @@
 #include <termios.h>
 #endif
 
+#if os(macOS)
+#include "libwebsockets.h"
+#else
 #include <libwebsockets.h>
+#endif
 
 //Some #define'd constants are unavailable in Swift; add them here to export them.
 static const int SWIFT_LWS_PRE_PADDING = LWS_SEND_BUFFER_PRE_PADDING;
