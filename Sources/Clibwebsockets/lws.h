4fc914c3acaf842ec409b9b719fd4d8ad1932a83
Alex Taffe <65547595+ataffeqeexo@users.noreply.github.com>
2020-10-28 09:35:29 -0400

    Prevent infinite nesting

--- Sources/Clibwebsockets/lws.h
+++ Sources/Clibwebsockets/lws.h
@@ -1,3 +1,6 @@
+#ifndef CLWS_H
+#define CLWS_H
+
 #include <openssl/ssl.h>
 
 #ifndef _WIN32
@@ -10,3 +13,5 @@
 static const int SWIFT_LWS_PRE_PADDING = LWS_SEND_BUFFER_PRE_PADDING;
 static const int SWIFT_LWS_POST_PADDING = LWS_SEND_BUFFER_POST_PADDING;
 //If more constants have the problem, add them here.
+
+#endif
