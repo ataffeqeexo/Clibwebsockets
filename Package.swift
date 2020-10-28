a87dea9f31688bdebff70c595748db33a93ac3f0
Alex Taffe <65547595+ataffeqeexo@users.noreply.github.com>
2020-10-28 09:41:06 -0400

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -6,9 +6,9 @@ let package = Package(
     name: "Clibwebsockets",
     products: [.library(name: "Clibwebsockets", targets: ["Clibwebsockets"])],
     dependencies: [
-        .package(url: "https://github.com/ataffeqeexo/COpenSSL.git", from: "0.3.0"),
-        .package(url: "https://github.com/ataffeqeexo/Clibuv.git", from: "0.1.0"),
-        .package(url: "https://github.com/ataffeqeexo/Clibevent.git", from: "0.1.0")
+        .package(url: "https://github.com/ataffeqeexo/COpenSSL.git", from: "0.4.0"),
+        .package(url: "https://github.com/ataffeqeexo/Clibuv.git", from: "0.2.0"),
+        .package(url: "https://github.com/ataffeqeexo/Clibevent.git", from: "0.2.0")
     ],
     targets: [
         .systemLibrary(
